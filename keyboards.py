a3cfef7e0536fa5da951d4cb4f5b4c6e7c721656
yousefzas83-pixel <yousefzas83@gmail.com>
2026-04-27 09:18:44 +0300

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-# كيبورد تجريبي بسيط
-main_kb = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="مرحباً")]
-], resize_keyboard=True)
-
